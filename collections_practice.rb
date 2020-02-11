ba7d45d2cfc9c255ee3efb479af8c3d5b7c47b57
79ceca7e-1710-4439-8f8b-c115f316e9e9 <github-enterprise+79ceca7e-1710-4439-8f8b-c115f316e9e9@flatironschool.com>
2020-02-11 13:21:49 -0800

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
 
+def sort_array_asc(arr)
+  arr.sort { |a, b| a <=> b }
+end
+
+def sort_array_desc(arr)
+  arr.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(arr)
+  arr.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(arr)
+  temp = arr[1]
+  arr[1] = arr[2]
+  arr[2] = temp
+  arr
+end
+
+def swap_elements_from_to(arr, index, d_index)
+  temp = arr[index]
+  arr[index] = arr[d_index]
+  arr[d_index] = temp
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.each { |element| element[2] = "$" }
+  return arr
+end
+
+def find_a(arr)
+  arr.select { |element| element.start_with?("a") }
+end
+
+def sum_array(arr)
+  arr.inject(:+)
+end
+
+def add_s(arr)
+  arr.each_with_index.map{ |el, i| i != 1 ? el << "s" : el }
+end
