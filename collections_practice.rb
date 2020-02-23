8b8c49c046f8de3d22161f0c25446885d5695595
Abdul Khan <akhan17@outlook.com>
2020-02-23 22:46:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(some_array)
+  sorted_array = some_array.sort
+  return sorted_array
+end
 
+def sort_array_desc(some_array)
+  desend_array = some_array.sort.reverse
+  return desend_array
+end
+
+def sort_array_char_count(some_array)
+  sorted_array = some_array.sort do |a,b| a.upcase <=> b.upcase end
+  return sorted_array
+end
+
+def swap_elements(some_array, a = 1, b = 2)
+  some_array[a], some_array[b] = some_array[b], some_array[a]
+  return some_array
+end
+
+def reverse_array(some_array, a =0 , b= -1)
+  some_array[a], some_array[b] = some_array[b], some_array[a]
+  return some_array
+end
+
+def kesha_maker(some_array)
+  some_array.each {|x| x[2]= "$"}
+  return some_array
+end
